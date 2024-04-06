7f59248c10a665c1f5a374be7298d57eb9969c93
Ennypitan Adekoya <ennypitan@gmail.com>
2024-04-06 23:47:06 +0100

    updated

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -9,8 +9,8 @@ function HeroSection() {
     <div className={`${styles["flex-row"]} ${styles["hero-section"]}`}>
       <div className={styles["flex-container"]}>
         <div className={styles["left-card"]}>
-          <p className={styles["hero_tag"]}> Dress to impress</p>
-          <p className={styles["hero_text"]}>handcrafted traditional attire!</p>
+          <p className={styles["hero_tag"]}> Dress to impress.</p>
+          <p className={styles["hero_text"]}>Handcrafted Traditional Attire</p>
           <p className={styles["hero_under_text"]}>
             No allow anyone tell you say Semo and Amala are for special guests.
             Dress like say na you be the chairperson of the occassion.
