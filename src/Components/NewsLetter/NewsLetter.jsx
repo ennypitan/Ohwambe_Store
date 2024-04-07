4b04afec6c31afa9942d1366ae13653e79697a4f
Ennypitan Adekoya <ennypitan@gmail.com>
2024-04-07 02:47:05 +0100

    updated

--- src/Components/NewsLetter/NewsLetter.jsx
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -3,14 +3,23 @@ import newImg from "../Assets/newletterImg.jpg";
 const NewsLetter = () => {
   return (
     <>
-      <div className={`${styles["flex-row"]} ${styles["newsletter-section"]}`}>
+      <div className={`${styles["flex-row"]} ${styles["newsletter__section"]}`}>
         <div className={styles["flex-container"]}>
           <div className={styles["news__lcard"]}>
-            <div className="img__holder">
+            <div className={styles["img__holder"]}>
               <img src={newImg} alt="" />
             </div>
           </div>
-          <div className={styles["news__rcard"]}>right news letter</div>
+          <div className={styles["news__rcard"]}>
+            <div className={styles["news__card__text"]}>
+              Get Exclusive Offers On Your Email
+            </div>
+            <p>Subscribe to our news later and stay updated</p>
+            <form action="">
+              <input type="text" placeholder="Enter your email" />
+              <input type="submit" value="Subscribe" />
+            </form>
+          </div>
         </div>
       </div>
     </>
