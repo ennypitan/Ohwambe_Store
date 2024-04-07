c23f97432ee06a167a1d9c85869d9561babd8f2f
Ennypitan Adekoya <ennypitan@gmail.com>
2024-04-07 15:34:22 +0100

    updated

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,6 +5,7 @@ import {
   FaSquareXTwitter,
   FaSquarePinterest,
   FaSquareWhatsapp,
+  FaSquareGithub,
 } from "react-icons/fa6";
 
 const Footer = () => {
@@ -36,6 +37,21 @@ const Footer = () => {
               <FaSquareWhatsapp style={{ width: "2rem", height: "2rem" }} />
             </li>
           </ul>
+        </div>
+        <div className={styles["under__footer"]}>
+          Copyright @ 2024 - Developed by Ennypitan
+          <li>
+            <a href="https://twitter.com/Ennipitan" target="_blank">
+              <FaSquareXTwitter
+                style={{ width: "1.5rem", height: "1.5rem", color: "black" }}
+              />
+            </a>
+            <a href="https://github.com/ennypitan" target="_blank">
+              <FaSquareGithub
+                style={{ width: "1.5rem", height: "1.5rem", color: "black" }}
+              />
+            </a>
+          </li>
         </div>
       </div>
     </>
