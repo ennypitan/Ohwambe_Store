0bbac87a3e4803f0694332f44f64e7d28d47a596
Ennypitan Adekoya <ennypitan@gmail.com>
2024-04-11 14:44:22 +0100

    updated

--- src/Components/StarRating/StarRating.jsx
+++ src/Components/StarRating/StarRating.jsx
@@ -1,23 +1,39 @@
-import React from "react";
-import { FaStar } from "react-icons/fa6";
+import React, { useState } from "react";
+import styles from "./starRating.module.css";
+import { FaStar, FaRegStar } from "react-icons/fa6";
 
-function StarRating({ rating }) {
-  function range(start, end, step = 1) {
-    let output = [];
+function StarRating() {
+  const [rating, setRating] = useState(null);
+  //   function range(start, end, step = 1) {
+  //     let output = [];
 
-    if (typeof end === "undefined") {
-      end = start;
-      start = 0;
-    }
-    for (let i = start; i < end; i += step) {
-      output.push(i);
-    }
-    return output;
-  }
+  //     if (typeof end === "undefined") {
+  //       end = start;
+  //       start = 0;
+  //     }
+  //     for (let i = start; i < end; i += step) {
+  //       output.push(i);
+  //     }
+  //     return output;
+  //   }
   return (
     <div>
-      {range(rating).map((star) => {
-        return <FaStar key={crypto.randomUUID()} color="#fe9923" />;
+      {[...Array(5)].map((star, i) => {
+        const starValue = i + 1;
+        return (
+          <label>
+            <input
+              type="radio"
+              name="rating"
+              value={starValue}
+              onClick={() => setRating(starValue)}
+            />
+            <FaStar
+              className={styles.star}
+              color={starValue <= rating ? "#ffc107" : "#e4e5e9"}
+            />
+          </label>
+        );
       })}
     </div>
   );
