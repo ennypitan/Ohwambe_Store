84e0d1c737ce695dddfcbb05eccc7731e222b27b
Ennypitan Adekoya <ennypitan@gmail.com>
2024-04-29 15:36:15 +0100

    update

---
+++ src/Components/Assets/all_products.js
@@ -0,0 +1,27 @@
+const all_products = [
+  {
+    id: 1,
+    name: "Iro and Buba",
+    category: "men",
+    image: image_1,
+    new_price: "35,000",
+    old_price: "45,000",
+  },
+  {
+    id: 2,
+    name: "Iro and Buba",
+    category: "women",
+    image: image_1,
+    new_price: "35,000",
+    old_price: "45,000",
+  },
+  {
+    id: 3,
+    name: "Iro and Buba",
+    category: "kid",
+    image: image_1,
+    new_price: "35,000",
+    old_price: "45,000",
+  },
+];
+export default all_products;
