81ace08e78a65ba11cf96147c179765878a993ce
Ennypitan Adekoya <ennypitan@gmail.com>
2024-05-02 23:35:44 +0100

    update

--- src/Components/Assets/all_products.js
+++ src/Components/Assets/all_products.js
@@ -1,3 +1,17 @@
+import kidImg1 from "./item_img_kid1.jpg";
+import kidImg2 from "./all_kids2.jpg";
+import kidImg3 from "./all_kids3.jpg";
+import kidImg4 from "./all_kids4.jpg";
+import kidImg5 from "./all_kids5.jpg";
+import kidImg6 from "./all_kids6.jpg";
+import kidImg7 from "./all_kids7.jpg";
+import kidImg8 from "./all_kids8.jpg";
+import kidImg9 from "./all_kids9.jpg";
+import kidImg10 from "./all_kids10.jpg";
+import kidImg11 from "./all_kids11.jpg";
+import menImg1 from "./all_men1.jpg";
+import menImg2 from "./all_men2.jpg";
+
 const all_products = [
   {
     id: 1,
