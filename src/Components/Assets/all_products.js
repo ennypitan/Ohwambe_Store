fd379efd1b3f04648adbe5163cabbc511812828f
Ennypitan Adekoya <ennypitan@gmail.com>
2024-05-16 04:42:25 +0100

    update

--- src/Components/Assets/all_products.js
+++ src/Components/Assets/all_products.js
@@ -223,6 +223,14 @@ const all_products = [
     new_price: "35,000",
     old_price: "45,000",
   },
+  {
+    id: 26,
+    name: "Iro and Buba",
+    category: "kid",
+    image: image_1,
+    new_price: "35,000",
+    old_price: "45,000",
+  },
 ];
 
 export default all_products;
